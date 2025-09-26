b428cc65249c284d00ffc4985395a080d362d5e5
epd <epd@sib.swiss>
2025-09-26 10:20:49 +0200

    Test mountings

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,22 @@
 <?php include("header.php"); ?>
+<?php
+    function is_mounted($species) {
+        if ( count(glob("/mnt/genome/$species/*.seq")) >= 2 ){
+            return '<span style="color:green; font-weight:bold">MOUNTED</span>';
+        }
+        return '<span style="color:red; font-weight:bold">NOT MOUNTED</span>';
+    }
+
+    function check_mounting() {
+        if ( count(glob("/mnt/genome/*/")) == 0 ){
+            return '<div style="text-align:center; color:red; font-weight:bold">No mounted directory</div>';
+        }
+        elseif ( count(glob("/mnt/genome/*/*.seq")) == 0 ){
+            return '<div style="text-align:center; color:red; font-weight:bold">No genome mounted!<br>Mount genomes of your choice - available <a href="https://epd.expasy.org/ftp/epdnew/docker_data/">here</a> - to have a full experience</div>';
+        }
+        return;
+    }
+?>
 
 <!-- News statement at the top of the page -->
 <div class="news" style="border-style:solid; border-width:1px; border-color:darkgrey; padding:5px">
@@ -24,6 +42,7 @@ with in-house analysis of promoter-specific high-throughput data for
 selected organisms only. This process gives EPDnew <a target="_blank" rel="noopener" href="https://doi.org/10.1093/nar/gks1233">high
 precision and high coverage</a>.
 </p>
+<?php echo check_mounting(); ?>
 <p style="margin-top:1cm">
 <table>
   <tr>
@@ -41,63 +60,92 @@ precision and high coverage</a>.
   <ul>
   <li>Animals:
     <ul>
-  <li><b><i>Homo sapiens</i></b>: <b> <?php echo
+  <li><b><i>Homo sapiens</i></b> (hg38): <b> <?php echo
       count(file("ftp/epdnew/human/current/Hs_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Macaca mulatta</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('hg38');?></li>
+  <li><b><i>Macaca mulatta</i></b> (rheMac8): <b><?php echo
       count(file("ftp/epdnew/M_mulatta/current/Rm_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Mus musculus</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('M_mulatta');?></li>
+  <li><b><i>Mus musculus</i></b> (mm10): <b><?php echo
       count(file("ftp/epdnew/mouse/current/Mm_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Rattus norvegicus</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('mm10');?></li>
+  <li><b><i>Rattus norvegicus</i></b> (rn6): <b><?php echo
       count(file("ftp/epdnew/R_norvegicus/current/Rn_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Gallus gallus</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('rn6');?></li>
+  <li><b><i>Gallus gallus</i></b> (galGal5): <b><?php echo
       count(file("ftp/epdnew/G_gallus/current/Gg_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Canis familiaris</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('galGal5');?></li>
+  <li><b><i>Canis familiaris</i></b> (canFam3): <b><?php echo
       count(file("ftp/epdnew/C_familiaris/current/Cf_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Drosophila melanogaster</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('canFam3');?></li>
+  <li><b><i>Drosophila melanogaster</i></b> (dm6): <b><?php echo
       count(file("ftp/epdnew/drosophila/current/Dm_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Apis mellifera</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('dm6');?></li>
+  <li><b><i>Apis mellifera</i></b> (amel5): <b><?php echo
       count(file("ftp/epdnew/A_mellifera/current/Am_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Danio rerio</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('amel5');?></li>
+  <li><b><i>Danio rerio</i></b> (danRer7): <b><?php echo
       count(file("ftp/epdnew/zebrafish/current/Dr_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Caenorhabditis elegans</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('danRer7');?></li>
+  <li><b><i>Caenorhabditis elegans</i></b> (ce6): <b><?php echo
       count(file("ftp/epdnew/worm/current/Ce_EPDnew.fps"));?></b>
-      promoters;</li>
+      promoters <?php echo is_mounted('ce6');?></li>
     </ul>
   </li>
   <li>Plants:
     <ul>
-    <li><b><i>Arabidopsis thaliana</i></b>: <b><?php echo
+    <li><b><i>Arabidopsis thaliana</i></b> (araTha1): <b><?php echo
       count(file("ftp/epdnew/arabidopsis/current/At_EPDnew.fps"));?></b>
-      promoters;</li>
-    <li><b><i>Zea mays</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('A_thaliana');?></li>
+    <li><b><i>Zea mays</i></b> (zm3): <b><?php echo
       count(file("ftp/epdnew/Z_mays/current/Zm_EPDnew.fps"));?></b>
-      promoters;</li>
+      promoters <?php echo is_mounted('zm3');?></li>
     </ul>
   </li>
   <li>Fungi:
     <ul>
-    <li><b><i>Saccharomyces cerevisiae</i></b>: <b><?php echo
+    <li><b><i>Saccharomyces cerevisiae</i></b> (sacCer3): <b><?php echo
       count(file("ftp/epdnew/S_cerevisiae/current/Sc_EPDnew.fps"));?></b>
-      promoters,</li>
-  <li><b><i>Schizosaccharomyces pombe</i></b>: <b><?php echo
+      promoters <?php echo is_mounted('sacCer3');?></li>
+  <li><b><i>Schizosaccharomyces pombe</i></b> (spo2): <b><?php echo
       count(file("ftp/epdnew/S_pombe/current/Sp_EPDnew.fps"));?></b>
-      promoters;</li>
+      promoters <?php echo is_mounted('S_pombe');?></li>
     </ul>
   </li>
   <li>Invertebrates:
     <ul>
-    <li><b><i>Plasmodium falciparum</i></b>: <b><?php echo
+    <li><b><i>Plasmodium falciparum</i></b> (pfa2): <b><?php echo
       count(file("ftp/epdnew/P_falciparum/current/Pf_EPDnew.fps"));?></b>
-      promoters.</li>
+      promoters <?php echo is_mounted('pfa2');?></li>
+    </ul>
+  </li>
+  </ul>
+      <strong>EPD</strong>
+  <ul>
+  <li>Animals:
+    <ul>
+  <li><b><i>Homo sapiens</i></b> (hg18):
+      <?php echo is_mounted('hg18');?></li>
+  <li><b><i>Pan troglodytes</i></b> (panTro2):
+      <?php echo is_mounted('panTro2');?></li>
+  <li><b><i>Mus musculus</i></b> (mm9):
+      <?php echo is_mounted('mm9');?></li>
+  <li><b><i>Rattus norvegicus</i></b> (rn4):
+      <?php echo is_mounted('rn4');?></li>
+  <li><b><i>Gallus gallus</i></b> (galGal3):
+      <?php echo is_mounted('galGal3');?></li>
+  <li><b><i>Bos taurus</i></b> (bosTau3):
+      <?php echo is_mounted('bosTau3');?></li>
+  <li><b><i>Drosophila melanogaster</i></b> (dm3):
+      <?php echo is_mounted('dm3');?></li>
+  <li><b><i>Caenorhabditis elegans</i></b> (ce6):
+      <?php echo is_mounted('ce6');?></li>
+    </ul>
+  </li>
+  <li>Plants:
+    <ul>
+    <li><b><i>Arabidopsis thaliana</i></b> (araTha1):
+      <?php echo is_mounted('A_thaliana');?></li>
     </ul>
   </li>
   </ul>
